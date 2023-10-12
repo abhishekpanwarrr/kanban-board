cd381c9240b2399c6c92f726cfebed39a2a7695d
Abhishek <40629087+abhishekpanwarrr@users.noreply.github.com>
2023-10-12 23:44:24 +0530

    added drag context

--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import { useMemo, useState } from "react";
 import PlusIcon from "../icons/PlusIcon";
 import { Column, Id } from "../types";
 import ColumnContainer from "./ColumnContainer";
+import { DndContext, DragStartEvent } from "@dnd-kit/core";
+import { SortableContext } from "@dnd-kit/sortable";
 
 type Props = {};
 
 const KanbanBoard = (props: Props) => {
   const [columns, setColumns] = useState<Column[]>([]);
+  const columnsId = useMemo(() => columns.map((col) => col.id), [columns]);
 
   const createNewColumn = () => {
     const columnToAdd: Column = {
@@ -19,26 +22,33 @@ const KanbanBoard = (props: Props) => {
     const filterColumn = columns.filter((col) => col.id !== id);
     setColumns(filterColumn);
   };
+  const onDragStart = (event: DragStartEvent) => {
+    console.log("drag start event", event);
+  };
   return (
     <div className="flex m-auto items-center w-full min-h-screen overflow-x-auto overflow-y-hidden px-[40px]">
-      <div className="m-auto flex gap-4">
-        <div className="flex gap-4">
-          {columns.map((column) => (
-            <ColumnContainer
-              column={column}
-              key={column.id}
-              deleteColumn={deleteColumn}
-            />
-          ))}
+      <DndContext onDragStart={onDragStart}>
+        <div className="m-auto flex gap-4">
+          <div className="flex gap-4">
+            <SortableContext items={columnsId}>
+              {columns.map((column) => (
+                <ColumnContainer
+                  column={column}
+                  key={column.id}
+                  deleteColumn={deleteColumn}
+                />
+              ))}
+            </SortableContext>
+          </div>
+          <button
+            onClick={createNewColumn}
+            className="h-[60px] w-[350px] min-w-[350px] cursor-pointer rounded-lg bg-mainBackgroundColor border border-columnBackgroundColor p-4 ring-rose-500 hover:ring-2 flex gap-2"
+          >
+            <PlusIcon />
+            Add Column
+          </button>
         </div>
-        <button
-          onClick={createNewColumn}
-          className="h-[60px] w-[350px] min-w-[350px] cursor-pointer rounded-lg bg-mainBackgroundColor border border-columnBackgroundColor p-4 ring-rose-500 hover:ring-2 flex gap-2"
-        >
-          <PlusIcon />
-          Add Column
-        </button>
-      </div>
+      </DndContext>
     </div>
   );
 };
