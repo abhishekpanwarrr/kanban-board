5566269b9db665d8a32dc19e26f95d6eed51f047
Abhishek <40629087+abhishekpanwarrr@users.noreply.github.com>
2023-10-17 19:46:25 +0530

    completed the app

--- src/components/ColumnContainer.tsx
+++ src/components/ColumnContainer.tsx
@@ -5,26 +5,32 @@ import { CSS } from "@dnd-kit/utilities";
 import { useMemo, useState } from "react";
 import PlusIcon from "../icons/PlusIcon";
 import TaskCard from "./TaskCard";
-interface ColumnContainerProps {
+
+interface Props {
   column: Column;
   deleteColumn: (id: Id) => void;
   updateColumn: (id: Id, title: string) => void;
+
   createTask: (columnId: Id) => void;
-  deleteTask: (id: Id) => void;
   updateTask: (id: Id, content: string) => void;
+  deleteTask: (id: Id) => void;
   tasks: Task[];
 }
-const ColumnContainer = (props: ColumnContainerProps) => {
-  const {
-    column,
-    deleteColumn,
-    updateColumn,
-    createTask,
-    tasks,
-    deleteTask,
-    updateTask,
-  } = props;
+
+function ColumnContainer({
+  column,
+  deleteColumn,
+  updateColumn,
+  createTask,
+  tasks,
+  deleteTask,
+  updateTask,
+}: Props) {
   const [editMode, setEditMode] = useState(false);
+
+  const tasksIds = useMemo(() => {
+    return tasks.map((task) => task.id);
+  }, [tasks]);
 
   const {
     setNodeRef,
@@ -36,52 +42,100 @@ const ColumnContainer = (props: ColumnContainerProps) => {
   } = useSortable({
     id: column.id,
     data: {
-      type: "Columns",
+      type: "Column",
       column,
     },
     disabled: editMode,
   });
+
   const style = {
     transition,
     transform: CSS.Transform.toString(transform),
   };
+
   if (isDragging) {
     return (
       <div
         ref={setNodeRef}
         style={style}
-        className=" bg-columnBackgroundColor w-[350px] h-[500px] max-h-[500px] rounded-md flex flex-col opacity-60 border-2 border-rose-500"
+        className="
+      bg-columnBackgroundColor
+      opacity-40
+      border-2
+      border-pink-500
+      w-[350px]
+      h-[500px]
+      max-h-[500px]
+      rounded-md
+      flex
+      flex-col
+      "
       ></div>
     );
   }
-  const taskIds = useMemo(() => {
-    return tasks.map((task) => task.id);
-  }, [tasks]);
+
   return (
     <div
       ref={setNodeRef}
       style={style}
-      className=" bg-columnBackgroundColor w-[350px] h-[500px] max-h-[500px] rounded-md flex flex-col"
+      className="
+  bg-columnBackgroundColor
+  w-[350px]
+  h-[500px]
+  max-h-[500px]
+  rounded-md
+  flex
+  flex-col
+  "
     >
       {/* Column title */}
       <div
         {...attributes}
         {...listeners}
-        onClick={() => setEditMode(true)}
-        className=" bg-mainBackgroundColor text-md h-[60px] cursor-grab rounded-md rounded-b-none p-3 font-bold border-columnBackgroundColor border-4 flex items-center justify-between"
+        onClick={() => {
+          setEditMode(true);
+        }}
+        className="
+      bg-mainBackgroundColor
+      text-md
+      h-[60px]
+      cursor-grab
+      rounded-md
+      rounded-b-none
+      p-3
+      font-bold
+      border-columnBackgroundColor
+      border-4
+      flex
+      items-center
+      justify-between
+      "
       >
         <div className="flex gap-2">
-          <div className="flex justify-center items-center bg-columnBackgroundColor px-2 py-1 text-sm rounded-full">
+          <div
+            className="
+        flex
+        justify-center
+        items-center
+        bg-columnBackgroundColor
+        px-2
+        py-1
+        text-sm
+        rounded-full
+        "
+          >
             0
           </div>
           {!editMode && column.title}
           {editMode && (
             <input
-              className="bg-black focus:border-rose-500 border-rounded outline-none px-2"
+              className="bg-black focus:border-rose-500 border rounded outline-none px-2"
               value={column.title}
               onChange={(e) => updateColumn(column.id, e.target.value)}
               autoFocus
-              onBlur={() => setEditMode(false)}
+              onBlur={() => {
+                setEditMode(false);
+              }}
               onKeyDown={(e) => {
                 if (e.key !== "Enter") return;
                 setEditMode(false);
@@ -90,15 +144,25 @@ const ColumnContainer = (props: ColumnContainerProps) => {
           )}
         </div>
         <button
-          onClick={() => deleteColumn(column.id)}
-          className="stroke-gray-500 hover:stroke-white hover:bg-columnBackgroundColor rounded px-1 py-2"
+          onClick={() => {
+            deleteColumn(column.id);
+          }}
+          className="
+        stroke-gray-500
+        hover:stroke-white
+        hover:bg-columnBackgroundColor
+        rounded
+        px-1
+        py-2
+        "
         >
           <TrashIcon />
         </button>
       </div>
+
       {/* Column task container */}
       <div className="flex flex-grow flex-col gap-4 p-2 overflow-x-hidden overflow-y-auto">
-        <SortableContext items={taskIds}>
+        <SortableContext items={tasksIds}>
           {tasks.map((task) => (
             <TaskCard
               key={task.id}
@@ -116,10 +180,11 @@ const ColumnContainer = (props: ColumnContainerProps) => {
           createTask(column.id);
         }}
       >
-        <PlusIcon /> Add Task
+        <PlusIcon />
+        Add task
       </button>
     </div>
   );
-};
+}
 
 export default ColumnContainer;
--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -9,14 +9,10 @@ interface Props {
   deleteTask: (id: Id) => void;
   updateTask: (id: Id, content: string) => void;
 }
-const TaskCard = ({ task, deleteTask, updateTask }: Props) => {
-  const [mouseIsOver, setMouseIsOver] = useState(false);
-  const [editMode, setEditMode] = useState(false);
 
-  const toggleEditMode = () => {
-    setEditMode((prev) => !prev);
-    setMouseIsOver(false);
-  };
+function TaskCard({ task, deleteTask, updateTask }: Props) {
+  const [mouseIsOver, setMouseIsOver] = useState(false);
+  const [editMode, setEditMode] = useState(true);
 
   const {
     setNodeRef,
@@ -39,54 +35,77 @@ const TaskCard = ({ task, deleteTask, updateTask }: Props) => {
     transform: CSS.Transform.toString(transform),
   };
 
-  if (editMode) {
-    return (
-      <div
-        ref={setNodeRef}
-        {...attributes}
-        {...listeners}
-        className="bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items-center flex text-left rounded-xl hover:ring-2 hover:ring-inset hover:ring-rose-500 cursor-grab relative"
-        style={style}
-      >
-        <textarea
-          className="h-[90%] w-full resize-none border-none rounded bg-transparent text-white focus:outline-none"
-          value={task.content}
-          autoFocus
-          onBlur={toggleEditMode}
-          onKeyDown={(e) => {
-            if (e.key === "Enter" && e.shiftKey) toggleEditMode();
-          }}
-          onChange={(e) => updateTask(task.id, e.target.value)}
-        ></textarea>
-      </div>
-    );
-  }
+  const toggleEditMode = () => {
+    setEditMode((prev) => !prev);
+    setMouseIsOver(false);
+  };
+
   if (isDragging) {
     return (
       <div
         ref={setNodeRef}
         style={style}
-        className="bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items-center flex text-left rounded-xl border-2 border-rose-500 cursor-grab relative opacity-50"
+        className="
+        opacity-30
+      bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items-center flex text-left rounded-xl border-2 border-rose-500  cursor-grab relative
+      "
       />
     );
   }
+
+  if (editMode) {
+    return (
+      <div
+        ref={setNodeRef}
+        style={style}
+        {...attributes}
+        {...listeners}
+        className="bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items-center flex text-left rounded-xl hover:ring-2 hover:ring-inset hover:ring-rose-500 cursor-grab relative"
+      >
+        <textarea
+          className="
+        h-[90%]
+        w-full resize-none border-none rounded bg-transparent text-white focus:outline-none
+        "
+          value={task.content}
+          autoFocus
+          placeholder="Task content here"
+          onBlur={toggleEditMode}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && e.shiftKey) {
+              toggleEditMode();
+            }
+          }}
+          onChange={(e) => updateTask(task.id, e.target.value)}
+        />
+      </div>
+    );
+  }
+
   return (
     <div
       ref={setNodeRef}
+      style={style}
       {...attributes}
       {...listeners}
       onClick={toggleEditMode}
-      style={style}
       className="bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items-center flex text-left rounded-xl hover:ring-2 hover:ring-inset hover:ring-rose-500 cursor-grab relative task"
-      onMouseEnter={() => setMouseIsOver(true)}
-      onMouseLeave={() => setMouseIsOver(false)}
+      onMouseEnter={() => {
+        setMouseIsOver(true);
+      }}
+      onMouseLeave={() => {
+        setMouseIsOver(false);
+      }}
     >
-      <p className="my-auto h-[90%]  w-full overflow-y-auto overflow-x-hidden whitespace-pre-wrap">
+      <p className="my-auto h-[90%] w-full overflow-y-auto overflow-x-hidden whitespace-pre-wrap">
         {task.content}
       </p>
+
       {mouseIsOver && (
         <button
-          onClick={() => deleteTask(task.id)}
+          onClick={() => {
+            deleteTask(task.id);
+          }}
           className="stroke-white absolute right-4 top-1/2 -translate-y-1/2 bg-columnBackgroundColor p-2 rounded opacity-60 hover:opacity-100"
         >
           <TrashIcon />
@@ -94,6 +113,6 @@ const TaskCard = ({ task, deleteTask, updateTask }: Props) => {
       )}
     </div>
   );
-};
+}
 
 export default TaskCard;
